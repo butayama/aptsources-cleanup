78bdad64d8bc3ee15cf739cd13d144337e7c88cf
Buta Yama <butayama@gmail.com>
2020-02-13 23:28:35 +0100

    nach Testlauf

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3 -OEs
 # -*- coding: utf-8
 
-import os, sys
+import os
+import sys
+import runpy
+
 sys_path = sys.path
 if os.altsep:
-	sys_path = ( p.replace(os.altsep, os.sep) for p in sys_path )
-sys.path[:] = [ p for p in sys_path if 'site-packages' not in p.split(os.sep) ]
+    sys_path = (p.replace(os.altsep, os.sep) for p in sys_path)
+sys.path[:] = [p for p in sys_path if 'site-packages' not in p.split(os.sep)]
 
-import runpy
 runpy.run_module('aptsources_cleanup', run_name='__main__')
